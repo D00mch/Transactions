1eff29637b1f3dcc1841441d7286019330356ace
arturdumchev <arturdumchev@yandex.ru>
2017-01-20 01:51:30 +0300

    Use an ExchangerImp in ProdunctsInPoundsFabric through Interface

--- app/src/main/java/com/liverm0r/transactions/business/transactions/products_fabric/ProductsInPoundsFabric.java
+++ app/src/main/java/com/liverm0r/transactions/business/transactions/products_fabric/ProductsInPoundsFabric.java
@@ -4,6 +4,7 @@ package com.liverm0r.transactions.business.transactions.products_fabric;
 import android.support.annotation.NonNull;
 
 import com.liverm0r.transactions.business.transactions.exchange.ExchangeImp;
+import com.liverm0r.transactions.business.transactions.exchange.ICurrencyExchanger;
 import com.liverm0r.transactions.business.transactions.exchange.graph.CurrencyNode;
 import com.liverm0r.transactions.data.model.DetailTransaction;
 import com.liverm0r.transactions.data.model.Product;
@@ -20,7 +21,7 @@ import static com.liverm0r.transactions.common.NAMES.GBP;
 public class ProductsInPoundsFabric implements IProductsFabric {
 
 
-    private ExchangeImp mExchanger;
+    private ICurrencyExchanger mExchanger;
     //private HashMap<CurrencyNode, GraphAlgorithm> currencyToAlgorithmMap;
 
     @NonNull @Override
