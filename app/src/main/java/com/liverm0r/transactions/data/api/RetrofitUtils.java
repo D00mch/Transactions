a3f209e90078e927a59a343a1eaf48da6406de58
arturdumchev <arturdumchev@yandex.ru>
2017-02-15 23:14:50 +0300

    fix RetrofitUtils class to be final not abstract

--- app/src/main/java/com/liverm0r/transactions/data/api/RetrofitUtils.java
+++ app/src/main/java/com/liverm0r/transactions/data/api/RetrofitUtils.java
@@ -8,7 +8,7 @@ import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public abstract class RetrofitUtils {
+public final class RetrofitUtils {
 
     private RetrofitUtils() {
     }
